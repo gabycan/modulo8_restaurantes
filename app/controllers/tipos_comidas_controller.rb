5bf1da4ad30cfff28e9a0cd79e7ab0e3a6ddee50
maria gabriela cano portocarrero <mgabrielacano@gmail.com>
2021-10-16 13:45:02 -0300

    actualizando el navegador

--- app/controllers/tipos_comidas_controller.rb
+++ app/controllers/tipos_comidas_controller.rb
@@ -3,5 +3,20 @@ class TiposComidasController < ApplicationController
     # GET /tipos_comidas
     def listar
         @todos_los_tipos = TipoComida.all
+        @titulo_link = "Mensaje desde el controlador"
+    end
+
+    # GET /nuevo_tipo_comida
+    def nuevo
+        @nuevo_tipo_comida= TipoComida.new
+    end
+
+    #post /nuevo_tipo_comida
+    def crear
+        #guardar lo que llegue del formulario en la base de datos
+        datos_tipo_comida = params.require(:tipo_comida).permit(:tipo)
+        nuevo_tipo = TipoComida.new(datos_tipo_comida)
+        nuevo_tipo.save
+        redirect_to listar_tipos_comida_path
     end
 end
