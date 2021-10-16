121f718c1d372b4edddf987be7562a6c4f34ef18
maria gabriela cano portocarrero <mgabrielacano@gmail.com>
2021-10-16 14:12:01 -0300

    generando rutas web

--- app/controllers/tipos_comidas_controller.rb
+++ app/controllers/tipos_comidas_controller.rb
@@ -11,12 +11,21 @@ class TiposComidasController < ApplicationController
         @nuevo_tipo_comida= TipoComida.new
     end
 
-    #post /nuevo_tipo_comida
+    #POST /nuevo_tipo_comida
     def crear
         #guardar lo que llegue del formulario en la base de datos
         datos_tipo_comida = params.require(:tipo_comida).permit(:tipo)
         nuevo_tipo = TipoComida.new(datos_tipo_comida)
         nuevo_tipo.save
-        redirect_to listar_tipos_comida_path
+        redirect_to listar_tipos_comidas_path
+    end
+
+    #DELETE tipos_comidas/:id
+    def eliminar
+        #pasos para eliminar un registro
+        #1. buscar el registro por ID
+        tipo = TipoComida.find(params[:id])
+        #2. intentar eliminar el registro
+        tipo.destroy
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   #Rutas Tipos Comidas
-  get 'tipos_comidas', to: 'tipos_comidas#listar', as: 'listar_tipos_comida'
+  get 'tipos_comidas', to: 'tipos_comidas#listar', as: 'listar_tipos_comidas'
   get 'tipos_comidas/nuevo', to: 'tipos_comidas#nuevo', as: 'nuevo_tipo_comida'
     
   post 'nuevo_tipo_comida', to: 'tipos_comidas#crear'
