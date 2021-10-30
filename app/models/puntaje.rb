873425d55f2644014c220ab4ae6605de342c9b48
maria gabriela cano portocarrero <mgabrielacano@gmail.com>
2021-10-30 12:27:08 -0300

    agregando callbacks en los modelos

--- app/models/puntaje.rb
+++ app/models/puntaje.rb
@@ -1,2 +1,17 @@
+#Modell va en singular
 class Puntaje < ApplicationRecord
+    
+    before_validation :capitalizar
+
+    has_many :puntajes_platos
+    has_many :puntajes_restaurantes
+
+    
+    validates(:tipo, presence: true)
+    validates(:tipo, uniqueness: true)
+
+    private
+    def capitalizar
+        self.tipo.capitalize!
+    end
 end
