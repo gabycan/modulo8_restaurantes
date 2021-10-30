cc4c3dfbb1feb05e8391482dc1b25bf0ced00bfb
maria gabriela cano portocarrero <mgabrielacano@gmail.com>
2021-10-30 12:20:30 -0300

    limpiar el controlador de usuarios

--- app/models/restaurante.rb
+++ app/models/restaurante.rb
@@ -3,12 +3,15 @@ class Restaurante < ApplicationRecord
   has_many :platos
 
   has_many :invitaciones
-  has_many :usuarios, through: :invitaciones
+  #has_many :usuarios, through: :invitaciones
 
   has_many :promociones
-  has_many :usuarios, through: :promociones
+  #has_many :usuarios, through: :promociones
 
-  validates(:tipo, presence: true) 
-  validates(:tipo, uniqueness: true)
+  has_many :puntajes
+  #has_many :puntajes, through: :puntajes_restaurantes
+
+  validates(:nombre, presence: true, uniqueness: true)
+  validates(:tipo_comida_id, presence: true)
 
 end
